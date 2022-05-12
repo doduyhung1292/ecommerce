1cb88d5a326c95a4d4a10941f38ad2f30f8da474
doduyhung1292 <doduyhung1292@gmail.com>
2022-05-12 19:22:19 +0700

    first commit

---
+++ src/component/NavBar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
+
+export default function NavBar(params) {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+  <Container fluid>
+    <Navbar.Brand href="#" style = {{marginLeft: "40px", fontSize: '2.3rem', fontWeight: 700}}>FISHU</Navbar.Brand>
+    <NavDropdown title="Categories" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Electronics</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Jewelery</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Men's clothing</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4">Women's clothing</NavDropdown.Item>
+        </NavDropdown>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      >
+        <Form className="d-flex" style = {{marginLeft: "500px"}}>
+        <FormControl
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+      </Nav>
+      
+        <Nav.Link href="#action2">Đăng nhập</Nav.Link>
+        <Nav.Link href="#action2"><Button variant="info">Đăng ký</Button></Nav.Link>
+
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+        </div>
+    )
+}
---
+++ src/screen/Customer/Home.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import NavBar from '../../component/NavBar.js';
+import Nava from '../../component/Nav.js';
+import { Carousel, img, Button, Container, Row, Col, Card } from 'react-bootstrap';
+
+export default function Home() {
+    const [products, setProducts] = useState('');
+    useEffect(() => {
+    const fetchData = async () => {
+            await fetch('https://fakestoreapi.com/products?limit=10')
+                .then((res) => res.json())
+                .then((res) => {
+                    setProducts(res);
+                    console.log(res);}) 
+            }  
+    }, [])
+    const createRate = () => {
+        axios.get('https://fakestoreapi.com/products?limit=4')
+            .then(res => setProducts(res.data))
+            .catch(err => console.log(err))
+     }
+     createRate();
+     //console.log(products);
+
+    return (
+        <div>
+            <div>
+            	<NavBar />
+                <Container style = {{maxWidth: '100%'}}>
+                <Row >
+                <Carousel >
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src="//cdn.shopify.com/s/files/1/0563/9661/1673/files/slide-1.jpg?v=1651135036"
+                      alt="First slide"
+                      style = {{maxHeight: '38rem', maxWidth: '100%'}}
+                    />
+                    <Carousel.Caption>
+                      <h3 style = {{color: '#221e1e', letterSpacing: '5px', fontSize: '3rem'}}>
+                      Spring Collection</h3>
+                      <Button variant="info">Shop now</Button>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src="//cdn.shopify.com/s/files/1/0563/9661/1673/files/slide-2.jpg?v=1651135049"
+                      alt="Second slide"
+                      style = {{maxHeight: '38rem', maxWidth: '100%'}}
+                    />
+
+                    <Carousel.Caption>
+                      <h3 style = {{color: '#221e1e', letterSpacing: '5px', fontSize: '3rem'}}>
+                      Spring Collection</h3>
+                      <Button variant="info">Shop now</Button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                </Carousel>
+                </Row>
+            </Container>
+            </div>
+            <Container style = {{maxWidth: '100%', marginTop: '1.5rem'}}>
+                <Row>
+                    <Col xs={3}>
+                        <Nava />
+                    </Col>
+                    <Col xs={9}>
+                        <Container>
+                        <Row>
+                        {
+                        products && products.map((data, index) => (
+                            <Col xs={3}>
+                                <Card style={{ width: '13rem', height: '25rem'}}>
+                                  <Card.Img variant="top" src={data.image} 
+                                  style={{maxHeight: '15rem', maxWidth: '15rem'}}/>
+                                  <Card.Body style={{padding: 0, paddingTop: '16px'}}>
+                                    <Card.Text> 
+                                        <p style={{display: 'inline-block',
+                                    maxWidth: '10rem',  overflow: 'hidden',
+                                    textOverflow: 'ellipsis', whiteSpace: 'nowrap', margin: 0}}>
+                                            {data.title}
+                                        </p>
+                                    </Card.Text>
+                                    <Card.Text>
+                                      {data.price}
+                                    </Card.Text>
+                                    <Button variant="primary">Add to card</Button>
+                                    <Button variant="primary" style={{marginLeft: '1.5px'}}>More</Button>
+                                  </Card.Body>
+                                </Card>
+                            </Col>))}
+                        </Row>
+                        </Container>
+                    </Col>
+
+                </Row>
+            </Container>
+        </div>
+    )
+}
